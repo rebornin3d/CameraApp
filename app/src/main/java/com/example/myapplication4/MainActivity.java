3b57a3c3f7b56f0cc7f6c0006d7f09dff963dad9
florin <rebornin3d@gmail.com>
2023-03-12 02:32:13 +0100

    Vreau sa iau framurile din SurfaceView Interface si sa le transform in Bitmap objects via Canvas class - Approach sugerat de GPT.
    Am reusit sa iau input de la camera si sa-l pun pe ecran in alt branch deci GPT-u stie chestii dar aici imi da un cod care da erori la linia 75 in MainActivity.java :                                                                                                     java.lang.RuntimeException: Unable to start activity ComponentInfo{com.example.myapplication4/com.example.myapplication4.MainActivity}: java.lang.NullPointerException: Attempt to invoke virtual method 'int android.graphics.Canvas.getWidth()' on a null object reference
    
    Multam!

--- app/src/main/java/com/example/myapplication4/MainActivity.java
+++ app/src/main/java/com/example/myapplication4/MainActivity.java
@@ -1,26 +1,17 @@
 package com.example.myapplication4;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.myapplication4.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 
@@ -29,34 +20,102 @@ public class MainActivity extends AppCompatActivity {
 
     private CameraUtils mCameraUtils;
     private Camera mCamera;
-    private SurfaceView mPreview;
-    private SurfaceHolder mHolder;
+    private SurfaceView surfaceView;
+    private SurfaceHolder mHolder_1;
+
+
+    // Initialize variables
+    int frameCount = 0;
+    Bitmap[] frames = new Bitmap[10];
+    int[] redSum = new int[256];
+    int[] greenSum = new int[256];
+    int[] blueSum = new int[256];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e(TAG, "onCreate()");
         setContentView(R.layout.activity_main);
+
+
+        //Get Camera Ids
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+
+
+            String[] cameraIds = manager.getCameraIdList();
+
+
+            for (String element : cameraIds) {
+
+                System.out.println("cameraIds are : " + element);
+            }
+
+
+        } catch (CameraAccessException e) {
+            throw new RuntimeException(e);
+        }
+
 
         // Initialize the CameraUtils object
         mCameraUtils = new CameraUtils();
 
         // Find the SurfaceView for displaying the camera preview
-        mPreview = findViewById(R.id.preview);
+        surfaceView = findViewById(R.id.preview);
 
         // Set up the SurfaceHolder for the SurfaceView
-        mHolder = mPreview.getHolder();
-        mHolder.addCallback(new SurfaceHolder.Callback() {
+        mHolder_1 = surfaceView.getHolder();
+
+        // Lock the Canvas object to acquire the video frames
+        Canvas canvas = mHolder_1.lockCanvas();
+        Log.e(TAG, "mHolder_1.getSurface()" + mHolder_1.getSurface());
+
+        // Create a new Bitmap object with the same dimensions as the video frames
+          Bitmap bitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
+
+       //  Get the dimensions of the video frames
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+
+        holder.unlockCanvasAndPost(canvas);
+
+
+        mHolder_1.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
-                // Do nothing
+
+
+                try {
+                    mCamera.setPreviewDisplay(holder);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                mCamera.startPreview();
             }
+
 
             @Override
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
                 // Configure the camera parameters
                 Camera.Parameters parameters = mCamera.getParameters();
                 mCameraUtils.configureCameraParameters(parameters, width, height);
-                mCamera.setParameters(parameters);
+
+
+                mCamera.stopPreview();
+
+//
+
+                mCamera.setDisplayOrientation(90);
+                try {
+                    mCamera.setPreviewDisplay(holder);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                mCamera.startPreview();
+
+
             }
 
             @Override
@@ -64,11 +123,14 @@ public class MainActivity extends AppCompatActivity {
                 // Do nothing
             }
         });
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.e(TAG, "onResume()");
 
         // Open the camera
         if (mCamera == null) {
@@ -81,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Start the camera preview
         try {
-            mCamera.setPreviewDisplay(mHolder);
+            mCamera.setPreviewDisplay(mHolder_1);
             mCamera.startPreview();
         } catch (IOException e) {
             Log.e(TAG, "Failed to start camera preview: " + e.getMessage());
@@ -91,6 +153,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        Log.e(TAG, "onPause()");
 
         // Release the camera
         mCamera.stopPreview();
