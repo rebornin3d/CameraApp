0528acb89b69aabcf03e9c1293defe73bafd9760
florin <rebornin3d@gmail.com>
2023-03-16 01:35:13 +0100

    Se aude ceva!Se aude un fasait cand incepe si apoi dupa 5 secunde sau 5000 de milisecunde : val duration = 5000 // Length of the sound signal in milliseconds

--- app/src/main/java/com/example/myapplication11_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/myapplication11_kotlin/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.myapplication11_kotlin
 import android.app.Activity
 import android.content.Intent
 import android.graphics.*
+import android.media.AudioAttributes
+import android.media.AudioFormat
+import android.media.AudioTrack
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageView
+import java.lang.Math.PI
+import kotlin.math.sin
 
 class MainActivity : AppCompatActivity() {
     private val PICK_IMAGE_REQUEST = 1
@@ -44,7 +49,11 @@ class MainActivity : AppCompatActivity() {
     private fun grayscaleImage(imageUri: Uri) {
         val inputStream = contentResolver.openInputStream(imageUri)
         val originalBitmap = BitmapFactory.decodeStream(inputStream)
-        val grayscaleBitmap = Bitmap.createBitmap(originalBitmap.width, originalBitmap.height, Bitmap.Config.ARGB_8888)
+        val grayscaleBitmap = Bitmap.createBitmap(
+            originalBitmap.width,
+            originalBitmap.height,
+            Bitmap.Config.ARGB_8888
+        )
         val canvas = Canvas(grayscaleBitmap)
         val paint = Paint()
         val colorMatrix = ColorMatrix().apply { setSaturation(0f) }
@@ -60,10 +69,10 @@ class MainActivity : AppCompatActivity() {
         val bitmap = BitmapFactory.decodeStream(inputStream)
 
         val width = bitmap.width
-        System.out.println("width : "+ width);
+        System.out.println("width : " + width);
 
         val height = bitmap.height
-        System.out.println("height : "+ height);
+        System.out.println("height : " + height);
 
         val pixels = IntArray(width * height)
         System.out.println("pixels : " + pixels);
@@ -72,15 +81,57 @@ class MainActivity : AppCompatActivity() {
 
         // The `pixels` array now contains the grayscale pixel values of the image
 
-        // TODO: Convert the grayscale pixels to sound signal
-        // You can use the `pixels` array to generate a sound signal based on the grayscale values
+        val numChannels = 1 // Mono sound signal
+        //val numSamples = duration * sampleRate * numChannels / 1000 // Number of samples in the sound signal
+        val duration = 5000 // Length of the sound signal in milliseconds
+        val sampleRate = 44000 // or 8000
+        // Sample rate of the sound signal in Hz
+        val numSamples = duration * sampleRate / 1000000 // Number of samples in the sound signal
+        val sampleData = ShortArray(numSamples) // Array to store the sound samples
+
+
+        // Generate the sound samples based on the grayscale values
+        for (i in 0 until numSamples) {
+            val index = i * pixels.size / numSamples // Map the sample index to a pixel index
+            val grayscaleValue =
+                pixels[index] and 0xff // Extract the grayscale value from the pixel value
+            val frequency =
+                grayscaleValue * 20f // Map the grayscale value to a frequency in the range of 0-440 Hz
+            val amplitude = 32767f // Maximum amplitude for 16-bit signed PCM data
+            sampleData[i] = (amplitude * sin(2 * PI.toFloat() * frequency * i / sampleRate)).toInt()
+                .toShort() // Generate the sound sample as a sine wave
+            System.out.println("// Generate the sound samples based on the grayscale values");
+        }
+
+        // Create an AudioTrack object to play the sound signal
+        val audioTrack = AudioTrack.Builder()
+            .setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build()
+            )
+            .setAudioFormat(
+                AudioFormat.Builder()
+                    .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                    .setSampleRate(sampleRate)
+                    .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
+                    .build()
+            )
+            .setBufferSizeInBytes(numSamples * 2)
+            .build()
+
+        // Write the sound samples to the AudioTrack buffer and play the sound signal
+        audioTrack.write(sampleData, 0, numSamples)
+        audioTrack.play()
+        System.out.println("audioTrack.play()");
 
         imageView.setImageBitmap(bitmap)
-
-
-        System.out.println("bitmap.getPixels : " +  bitmap.getPixels(pixels, 0, width, 0, 0, width, height));
     }
+
+
 }
+
 
 
 
